05d4bf954a3003024840ab88a39f17ceffd6a885
Himanshu Kumar Singh <R10651@apexuniversity.co.in>
2025-08-15 11:59:05 +0530

    Proof of Work , cryptography and SHA256

---
+++ 01 Introduction/FindHashMapWithPrefix.js
@@ -0,0 +1,18 @@
+//Proof of Work
+const crypto = require("crypto");
+
+function findHashMapWithPrefix(prefix){
+    let input=0;
+    while(true){
+        let inputStr = input.toString();
+        let hash = crypto.createHash('sha256').update(inputStr).digest('hex');
+        if(hash.startsWith(prefix)){
+            return {input : inputStr , hash : hash};
+        }
+        input++;
+    }
+}
+
+const result = findHashMapWithPrefix('00000');
+console.log(`Input : ${result.input}`);
+console.log(`Hash : ${result.hash}`);
